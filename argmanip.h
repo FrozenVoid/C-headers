6ea2b0357cf9d830f813a17663421c39cdc52cd8
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-08 22:15:41 +0300

    Update argmanip.h

--- argmanip.h
+++ argmanip.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "dapply.h"
+#include "tapply.h"
 #include "chainapply.h"
 /* argmanip.h
 reverse(args) reverse argument order(1,2,3)-> 3,2,1
@@ -8,12 +9,24 @@ mergeall(args) merge all argument tokens
 toatom(args...) remove all commas from arglist
 opapply(op,args...) -> arg1 op arg2 op arg3 ...
 genargs(n,arg) repeat arg N times
+tolists(args...) converts arglist to list of tuples (1,2,3)->((1),(2),(3))
+appendall(tup,args...) ((a,b,c),1,2,3) ->((a,b,c,1),(a,b,c,2),(a,b,c,3))
+prefixall(tup,args...) ((a,b,c),1,2,3) ->((1,a,b,c),(2,a,b,c),(3,a,b,c))
 */
 #define reverse(args...) dapply(swapargs,id,args)
 #define applyall(func,args...) chainapply(func,args)
 #define mergeall(args...) dapply(merge,id,args)
 #define toatom(args...) dapply(remcomma,id,args)
 #define genargs(n,arg) merge(chainapply,n)(arg id,) 
+#define tolists(args...) chainapply(tuple,args)
+
+
+#define appendall1(tup,arg) mergetuples(tup,(arg))
+#define prefixall1(tup,arg) mergetuples((arg),tup)
+
+#define appendall(tup,args...) tapply(tup,appendall1,args)
+#define prefixall(tup,args...) tapply(tup,prefixall1,args)
+
 
 
 #define opapply(op,arg1,args...) arg1 dapply(remcomma,op id,args)
