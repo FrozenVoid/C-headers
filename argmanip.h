bb9d76b97e88519bf983f44f971665e981549eb3
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-08 15:29:13 +0300

    Update argmanip.h

--- argmanip.h
+++ argmanip.h
@@ -1,17 +1,19 @@
 #pragma once
 #include "dapply.h"
+#include "chainapply.h"
 /* argmanip.h
 reverse(args) reverse argument order(1,2,3)-> 3,2,1
 applyall(func,args) apply func to each argument-> func(arg1),func(arg2),...
 mergeall(args) merge all argument tokens
 toatom(args...) remove all commas from arglist
 opapply(op,args...) -> arg1 op arg2 op arg3 ...
+genargs(n,arg) repeat arg N times
 */
-#define reverse(args...) dapply(swap,id,args)
-#define applyall(func,args...) dapply(id,func,args)
+#define reverse(args...) dapply(swapargs,id,args)
+#define applyall(func,args...) chainapply(func,args)
 #define mergeall(args...) dapply(merge,id,args)
 #define toatom(args...) dapply(remcomma,id,args)
-
+#define genargs(n,arg) merge(chainapply,n)(arg id,) 
 
 
 #define opapply(op,arg1,args...) arg1 dapply(remcomma,op id,args)
