a4eac8c12d4522bb8017feaf6e6b5e0ae1be5ae1
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-08 13:47:10 +0300

    Update random.h

--- random.h
+++ random.h
@@ -2,16 +2,22 @@
 
 #include <stdint.h>
 /*
-raw numbers:
+
+
+raw numbers::
 randuint64() integer from 0 to UINT64_MAX
 randfloat() random float in range 0.0-1.0
-range(reccomended)
-frange (start,end) float in specific range
+range(reccomended)::
+rnd(start,end) macro the expand to one of functions below
+frange (start,end) float in specific range,if argument is floating point
 rrange(start,end) integer in specific range(use randuint64()&n for powers of 2 which will be faster than modulus due opcode latency)
+
 */
 
 
 /*xoroshiro random generator from http://prng.di.unimi.it/xoshiro256starstar.c */
+
+#define rnd(start,end) _Generic((end),double:frange,float:frange,long double:frange,default:rrange)(start,end)
 uint64_t xrotl(uint64_t x,uint32_t k){//compiles to ROTL
 return (x << k) | (x >> (64 - k));}
 
@@ -19,19 +25,19 @@ return (x << k) | (x >> (64 - k));}
 uint64_t randuint64(void) {
         static uint64_t s[4]={1,2,3,4};
 
-    const uint64_t result = xrotl(s[1] * 5, 7) * 9;
+	const uint64_t result = xrotl(s[1] * 5, 7) * 9;
 
-    const uint64_t t = s[1] << 17;
+	const uint64_t t = s[1] << 17;
 
-    s[2] ^= s[0];
-    s[3] ^= s[1];
-    s[1] ^= s[2];
-    s[0] ^= s[3];
+	s[2] ^= s[0];
+	s[3] ^= s[1];
+	s[1] ^= s[2];
+	s[0] ^= s[3];
 
-    s[2] ^= t;
+	s[2] ^= t;
 
-    s[3] = xrotl(s[3], 45);
-    return result;
+	s[3] = xrotl(s[3], 45);
+	return result;
 }
 
 uint64_t rrange(uint64_t start,uint64_t end){
@@ -46,5 +52,4 @@ double randfloat(void){ return uintdouble01(randuint64());}
 
 double frange(double start, double end){
  return (uintdouble01(randuint64())*(end-start))+start; }
-
 
