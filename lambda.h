8a37a289187e0990126522da17ee373a1ca25ca7
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-09 11:55:22 +0300

    Update lambda.h

--- lambda.h
+++ lambda.h
@@ -7,7 +7,13 @@ int res=lambda(int,(int a,int b),int c=b+a,return c*b*a)(2,3);
 print("result:",res,"\n");
 float res2=lambda(float,(float a,float b,float d),float c=b+a*d,return c*b*a)(4.0,3.5,4.401);
 print("result:",res2,"\n");
+----
+evtokens(args...) convert tokens to executable statements(last expr is returned)
+
+
 */
 #include "arguments.h"
 #include "argmanip.h"
 #define lambda(return_type,argument_tuple,body...) ({return_type lambda_func argument_tuple {opapply(;,body);}; lambda_func;})
+#define evtokens(args...) ({opapply(;,args);})
+
