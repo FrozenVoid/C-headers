4abeac54f3de7971962d9c0721f7825c54e8a8ea
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-11 13:17:19 +0300

    Update argmanip.h

--- argmanip.h
+++ argmanip.h
@@ -7,6 +7,7 @@ reverse(args) reverse argument order(1,2,3)-> 3,2,1
 applyall(func,args) apply func to each argument-> func(arg1),func(arg2),...
 mergeall(args) merge all argument tokens
 toatom(args...) remove all commas from arglist
+last(args...) return last argument
 opapply(op,args...) -> arg1 op arg2 op arg3 ...
 genargs(n,arg) repeat arg N times
 tolists(args...) converts arglist to list of tuples (1,2,3)->((1),(2),(3))
@@ -18,6 +19,10 @@ prefixall(tup,args...) ((a,b,c),1,2,3) ->((1,a,b,c),(2,a,b,c),(3,a,b,c))
 #define applyall(func,args...) chainapply(func,args)
 #define mergeall(args...) dapply(merge,id,args)
 #define toatom(args...) dapply(remcomma,id,args)
+
+#define firstarg(args...) first(args) //forwarding
+#define last(args...) firstarg(reverse(args))
+
 #define genargs(n,arg) merge(chainapply,n)(arg id,) 
 #define tolists(args...) chainapply(tuple,args)
 #define fromlists(args...) chainapply(detuple,args)
