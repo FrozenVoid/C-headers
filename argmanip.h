ed613c62dd8fa516051fe237472151f7a0cb342d
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-12 20:54:13 +0300

    Update argmanip.h

--- argmanip.h
+++ argmanip.h
@@ -11,11 +11,12 @@ last(args...) return last argument
 opapply(op,args...) -> arg1 op arg2 op arg3 ...
 genargs(n,arg) repeat arg N times
 tolists(args...) converts arglist to list of tuples (1,2,3)->((1),(2),(3))
-fromlist(args...) convert tuple list to arglist ((1,2),(3,4))->(1,2,3.4)
+fromlist(args...) convert tuple list to arglist ((1,2),(3,4))->(1,2,3,4)
 appendall(tup,args...) ((a,b,c),1,2,3) ->((a,b,c,1),(a,b,c,2),(a,b,c,3))
 prefixall(tup,args...) ((a,b,c),1,2,3) ->((1,a,b,c),(2,a,b,c),(3,a,b,c))
 rec2apply(func,args...) rec2apply(func,1,2,3,4)> func(1,func(2,func(3,4)));
 rec2applyt(func,(a,b,c),1,2,3,4,5)- > func (a,b,c)(1,func (a,b,c)(2,func (a,b,c)(3,func (a,b,c)(4,5))));
+dupapply(func,times,(1,2,3) apply func N times -> func((1,2,3)),...
 */
 #define reverse(args...) dapply(swapargs,id,args)
 #define applyall(func,args...) chainapply(func,args)
@@ -24,10 +25,13 @@ rec2applyt(func,(a,b,c),1,2,3,4,5)- > func (a,b,c)(1,func (a,b,c)(2,func (a,b,c)
 #define rec2apply(func,args...) dapply(func,id,args)
 #define rec2applyt(func,tup,args...) dapply(func tup,id,args)
 
+
 #define firstarg(args...) first(args) //forwarding
 #define last(args...) firstarg(reverse(args))
 
 #define genargs(n,arg) merge(chainapply,n)(arg id,) 
+
+#define dupapply(func,n,args_tup) chainapply(func,genargs(n,args_tup))
 #define tolists(args...) chainapply(tuple,args)
 #define fromlists(args...) chainapply(detuple,args)
 
