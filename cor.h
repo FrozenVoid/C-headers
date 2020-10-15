e52fc9d200c93d7207d82e74f5ff2f339ec73d7b
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-15 11:14:00 +0300

    Update cor.h

--- cor.h
+++ cor.h
@@ -3,6 +3,8 @@
 cbegin  - start coroutine block
 cret([arg]) - return a (optional) value, set to continue after this return
 cend([arg])  - end block with final return with optinal arg.
+creset([arg]) -  reset the switch, returning optional args(as cret),
+cbreak() - break out of switch and reset switch value.
 
 cbegin {
 //this block is within the coroutine switch.
@@ -10,15 +12,19 @@ cret([x])
 
 }cend([x])
 
+
 see https://www.chiark.greenend.org.uk/~sgtatham/coroutines.html
 */
-#define cbegin static typeof(__LINE__) c_switch_l = 0;\
+#define cbegin() static typeof(__LINE__) c_switch_l = 0;\
  switch(c_switch_l){  case 0:;
 
 #define cret(args...) do{\
  c_switch_l = __LINE__;return args;case __LINE__:;}\
 while(0)
-
+#define creset(args...) do{\
+ c_switch_l = 0;return args;}\
+while(0)
+#define cbreak() ;c_switch_l = 0;break;
 #define cend(args...) ;} return args;
 
 
