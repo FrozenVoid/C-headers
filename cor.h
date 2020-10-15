cddbb466e98e2c82a78912425adb28538ae10e3d
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-15 11:37:29 +0300

    Update cor.h

--- cor.h
+++ cor.h
@@ -5,6 +5,8 @@ cret([arg]) - return a (optional) value, set to continue after this return
 cend([arg])  - end block with final return with optinal arg.
 creset([arg]) -  reset the switch, returning optional args(as cret),
 cbreak() - break out of switch and reset switch value.
+cwait(cond,[args]) - return if condition not met,with optinal args.(saves location to switch value)
+cset(val) - set switch_line to specific value(e.g. cset(0) or cset(__LINE__)
 
 cbegin {
 //this block is within the coroutine switch.
@@ -24,7 +26,11 @@ while(0)
 #define creset(args...) do{\
  c_switch_l = 0;return args;}\
 while(0)
+#define cwait(cond,args...) do{\
+ c_switch_l = __LINE__;case __LINE__:;if(!(cond)) return args;}\
+while(0)
 #define cbreak() ;c_switch_l = 0;break;
+#define cset(v) ;c_switch_l = v;
 #define cend(args...) ;} return args;
 
 
