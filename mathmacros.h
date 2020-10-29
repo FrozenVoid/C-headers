1e921ab8d101ecfa4b284799eb68720f4800f420
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-10-29 09:58:32 +0000

    Update mathmacros.h

--- mathmacros.h
+++ mathmacros.h
@@ -4,3 +4,6 @@
 #define u64log2(X) ((unsigned) (8*sizeof (unsigned long long) - __builtin_clzll((X)) - 1))
 #define issigned(x) (((typeof(x))-1)<((typeof(x))0))
 #define isinteger(x) ((typeof(x))1.1 == 1)
+//branchless bound
+#define bound(x,minbound,maxbound) (((x)*((x)>minbound)*((x)<maxbound))\
+ +(((x)<minbound)*minbound)+(((x)>maxbound)*maxbound))
