862f6aaa76c225bc095c6fcab2cdd77083be2136
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-11-03 06:25:21 +0000

    Create new file

---
+++ void.h
@@ -0,0 +1,38 @@
+#pragma once //void.h include all headers
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fenv.h>
+#include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tgmath.h>
+#include <time.h>
+
+
+#include "argcount.h"
+#include "argmanip.h"
+#include "arguments.h"
+#include "boolean-tokens.h"
+#include "chainapply.h"
+#include "cor.h"
+#include "dapply.h"
+#include "enumclass.h"
+#include "lambda.h" 
+#include "mathmacros.h"
+#include "print.h"
+#include "random.h"
+#include "remtype.h"
+#include "size.h"
+#include "tapply.h"
+#include "timing.h"
