d16598ce16c5d362f8b33fb7e6e9f1aa4bf144dc
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-11-03 22:08:39 +0000

    assume compiler has empty structs extension(GCC-compatible)

--- enumclass.h
+++ enumclass.h
@@ -3,5 +3,5 @@
 create a strongly typed enum using unions.
 
 */
-#define enumc(name,args...) typedef union name{char v;}name;name args;
+#define enumc(name,args...) typedef union name{}name;name args;
 #define isenumeq(a,b) ((&a)==(&b))
