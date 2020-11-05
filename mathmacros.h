5af646bdca14bc8706bc4ac1c023e4aa87ae9a74
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-11-05 15:05:45 +0000

    add numberof macros

--- mathmacros.h
+++ mathmacros.h
@@ -1,6 +1,9 @@
 #pragma once 
 #include "lambda.h"
 /* mathmacros.h
+numberof(str) convert string to number
+hexnumberof(str) convert hex string to number
+
 fixedpoint(func,guess) find a fixed point value(or closest guess) for func where f(x)==x
 bound(x,min,max) bound variable by min/max which are returned if x is below/above
 isinteger(x) return 1 if integer,0 otherwise
@@ -9,6 +12,9 @@ u64log2(x) return interger log2 value of uint64_t number
 average(x,y) return average of x/y
 averagedamp(func) return function that returns average of x/func(x)
 */
+#define numberof(str) strtoll(str,NULL,10)
+#define hexnumberof(str) strtoll(str,NULL,16)
+
 #define u64log2(X) ((unsigned) (8*sizeof (unsigned long long) - __builtin_clzll((X)) - 1))
 #define issigned(x) (((typeof(x))-1)<((typeof(x))0))
 #define isinteger(x) ((typeof(x))1.1 == 1)
@@ -18,9 +24,14 @@ averagedamp(func) return function that returns average of x/func(x)
 
 #define average(x,y) ({typeof(x) sum=(x)+(y); sum/2;})
 #define averagedamp(func) lambda(double,(double val),return  average(val,func(val)))
+#define derivative(func) lambda(double,(double x), const double dx=1e6*DBL_EPSILON;   return ((func(x+dx))-func(x))/dx   )
+#define fixpointtranform(gfunc,transform,guess) fixedpoint(transform(gfunc),guess)
 
 #define fixedpoint(func,guess) ({double diff=1e300,result=guess,current=guess;;\
 while(1){result=func(current); \
 double thisdiff=fabs(result-current);\
 if(thisdiff>=diff){break;}\
 diff=thisdiff;current=result;} ;current;})
+
+#define newtontransform(gfunc) lambda(double,(double x),return x-( gfunc(x)/derivative(gfunc)(x)  ))
+#define newtonmethod(gfunc,guess) fixedpoint(newtontransform(gfunc),guess)
