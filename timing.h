0aa962c47f165b1993d19e7d44c69f152e7d3708
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-11-05 10:09:32 +0000

    add nanoseconds and walltime timings.

--- timing.h
+++ timing.h
@@ -1,8 +1,34 @@
 #pragma once
-/*timing.h time in cycles*/
-#include <x86intrin.h>
+#include <x86intrin.h>//__rdtsc
 #include <stdint.h>
+#include <time.h> //clock_gettime
 #include "argmanip.h"
-#define timing(args...) ({uint64_t start=__rdtsc();opapply(;,args);__rdtsc()-start;})
+/* walltime(args...) - (double precision) seconds of argument execution time
+nanoseconds(args...) - uint64_t nanoseconds of argument execution time
+microseconds(args...) - uint64_t microseconds of argument execution time
+milliseconds(args...) - uint64_t milliseconds of argument execution time
 
+timing(args...)uint64_t CPU clock cycles of argument execution time
+*/
+
+#define walltime(args...) ({ time_t start;time(&start);opapply(;,args);time_t end;time(&end);\
+ difftime(end,start);})
+#define nanoseconds(args...) ({struct timespec start, end;clock_gettime(CLOCK_MONOTONIC_RAW, &start);\
+opapply(;,args);clock_gettime(CLOCK_MONOTONIC_RAW, &end);\
+(end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);})
+
+#define microseconds(args...) ({struct timespec start, end;clock_gettime(CLOCK_MONOTONIC_RAW, &start);\
+opapply(;,args);clock_gettime(CLOCK_MONOTONIC_RAW, &end);\
+(end.tv_sec - start.tv_sec) * 1000000 + ((end.tv_nsec - start.tv_nsec)/1000);})
+
+#define milliseconds(args...) ({struct timespec start, end;clock_gettime(CLOCK_MONOTONIC_RAW, &start);\
+opapply(;,args);clock_gettime(CLOCK_MONOTONIC_RAW, &end);\
+(end.tv_sec - start.tv_sec) * 1000 + ((end.tv_nsec - start.tv_nsec)/1000000);})
+
+
+#define seconds(args...) ({struct timespec start, end;clock_gettime(CLOCK_MONOTONIC_RAW, &start);\
+opapply(;,args);clock_gettime(CLOCK_MONOTONIC_RAW, &end);\
+(end.tv_sec - start.tv_sec) ;})
+
+#define timing(args...) ({uint64_t start=__rdtsc();opapply(;,args);__rdtsc()-start;})
 
