d8036a585ceaca3894d222c54286e27ff534c6e4
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-11-28 15:49:07 +0200

    Update boolean-tokens.h

--- boolean-tokens.h
+++ boolean-tokens.h
@@ -1,7 +1,7 @@
 
 #pragma once
     /*  boolean-tokens.h perform boolean operations on tokens
-    x,y arguments must be tokens 1,0 
+    x,y arguments must be tokens 1,0
     returned token is 1 for true, and 0 for false
     Macros:
     bool_true(...) always returns 1
@@ -18,10 +18,10 @@
     bool_nor(x,y) returns (x NOR y)
 
     //en.wikipedia.org/wiki/Converse_implication
-    bool_thenif(x,y) returns (x then/if y) 
+    bool_thenif(x,y) returns (x then/if y)
 
     //en.wikipedia.org/wiki/Converse_nonimplication
-    bool_xq(x,y) returns (x Xq y) 
+    bool_xq(x,y) returns (x Xq y)
 
     //en.wikipedia.org/wiki/Material_implication_(rule_of_inference)
     bool_ifthen(x,y) returns (x ifthen y)
@@ -34,9 +34,17 @@
     bool_proj(x,y) returns (x proj y)
     bool_nproj(x,y) returns (x nproj y)
     bool_qproj(x,y) returns (x qproj y)
-    bool_nqproj(x,y) returns (x nqproj y) 
+    bool_nqproj(x,y) returns (x nqproj y)
 
+ typical use of boolean-tokens.h
 
+#define custom_switchA 0 //disable flag A
+#define custom_switchB 1 //enable flag B
+#define custom_switchC 1 //enable flag C
+#define bool_func(x,y,z)  bool_adj(x,bool_xor(y,z))
+
+if(bool_func(custom_switchA,custom_switchB,custom_switchC)){
+code to run;}
 
     */
     //==local merge macros
@@ -50,11 +58,11 @@
 
     #define bool_true(...) 1
     #define bool_false(...) 0
-    //==unary functions 
+    //==unary functions
     #define bool_id0 0
     #define bool_id1 1
     #define bool_id(v) bool_merge2(bool_id,v)
-    #define bool_neg0 1 
+    #define bool_neg0 1
     #define bool_neg1 0
     #define bool_neg(v) bool_merge2(bool_neg,v)
     //==Binary functions
@@ -79,7 +87,7 @@
     #define bool_or10 1
     #define bool_or11 1
     #define bool_or(x,y) bool_merge3(bool_or,x,y)
-    #define bool_nor(x,y) bool_neg(bool_or(x,y)) 
+    #define bool_nor(x,y) bool_neg(bool_or(x,y))
 
 
 
@@ -95,21 +103,23 @@
     #define bool_ifthen10 0
     #define bool_ifthen01 1
     #define bool_ifthen00 1
-    #define bool_ifthen(x,y) bool_merge3(bool_ifthen,x,y) 
-    #define bool_adj(x,y) bool_neg(bool_ifthen(x,y)) 
+    #define bool_ifthen(x,y) bool_merge3(bool_ifthen,x,y)
+    #define bool_adj(x,y) bool_neg(bool_ifthen(x,y))
 
 
     #define bool_proj11 1
     #define bool_proj10 1
     #define bool_proj01 0
     #define bool_proj00 0
-    #define bool_proj(x,y) bool_merge3(bool_proj,x,y) 
-    #define bool_nproj(x,y) bool_neg(bool_proj(x,y)) 
+    #define bool_proj(x,y) bool_merge3(bool_proj,x,y)
+    #define bool_nproj(x,y) bool_neg(bool_proj(x,y))
 
     #define bool_qproj11 1
     #define bool_qproj10 0
     #define bool_qproj01 1
     #define bool_qproj00 0
-    #define bool_qproj(x,y) bool_merge3(bool_qproj,x,y) 
-    #define bool_nqproj(x,y) bool_neg(bool_qproj(x,y)) 
+    #define bool_qproj(x,y) bool_merge3(bool_qproj,x,y)
+    #define bool_nqproj(x,y) bool_neg(bool_qproj(x,y))
+
+
 
