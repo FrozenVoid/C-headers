5ba8cf4594c3015e00b21e03593f85c78bcaef2d
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-01-28 06:20:25 +0200

    Update fileio.h

--- fileio.h
+++ fileio.h
@@ -3,4 +3,4 @@
 #include <stdio.h>
 #define filesize(fptr) ({ atype curpos=ftell(fptr);\
 fseek(fptr,0,SEEK_END);atype retsize=ftell(fptr);\
-fseek(fptr,curpose,SEEK_SET);retsize;})
+fseek(fptr,curpos,SEEK_SET);retsize;})
