cb5b568ff884ee2d193c8fc7ff241739317748d6
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-17 15:14:12 +0200

    Update fileio.h

--- fileio.h
+++ fileio.h
@@ -4,3 +4,5 @@
 #define filesize(fptr) ({ atype curpos=ftell(fptr);\
 fseek(fptr,0,SEEK_END);atype retsize=ftell(fptr);\
 fseek(fptr,curpos,SEEK_SET);retsize;})
+
+#define fextension(filename) ({ char *dot = strrchr(filename, '.');(!dot || dot == filename)?"":dot+1;})
