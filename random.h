8f4e1dc4628acb73eb52d327ad3317ff686b204e
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-21 22:28:41 +0200

    Update random.h

--- random.h
+++ random.h
@@ -96,4 +96,24 @@ d.b[0]=randuint64();d.b[1]=randuint64();d.a;})
  ((uintdouble01(randuint64())*(end1-start1))+start1);})
 
 
+static inline float
+fastlog2 (float x)
+{
+  union { float f; uint32_t i; } vx = { x };
+  union { uint32_t i; float f; } mx = { (vx.i & 0x007FFFFF) | 0x3f000000 };
+  float y = vx.i;
+  y *= 1.1920928955078125e-7f;
+
+  return y - 124.22551499f
+           - 1.498030302f * mx.f
+           - 1.72587999f / (0.3520887068f + mx.f);
+}
+
+static f32 entropy(u8* data,size_t len){
+ u64 freq[256]={0};f32 ent=len*fastlog2(len);
+for(size_t i=0;i<len;i++)freq[data[i]]++;
+for(size_t i=0;i<256;i++){
+ent-= freq[i] * (fastlog2(freq[i]));
+} return ent/len;}
+
 
