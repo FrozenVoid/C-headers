ad57c6b484fb126cff445873b01bfdaa53bc485a
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-23 12:29:49 +0200

    Update array.h

--- array.h
+++ array.h
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include "random.h"
 /*
+combsort(A,length) sort array with combsort
 reduce(func,arr,len)  return accumulated value from func(accum,arr[i])
 formap(func,arr,len) set len elements to func(arr[i])
 foreach(func,arr,len) execute func(arr[i]) for len elements
@@ -10,6 +11,13 @@ uniqueset(arr,len,rangestart) set len elements of array to rangestart+i
 uniquesetrand(arr,len,rangemin,rangemax) set array elements to unique values in range
 
 */
+    #define combsort(A,lenx) ({size_t i,j, gap=lenx,len=gap;;;\
+    do{gap = gap<10 ? gap-1 : (gap*7)/9+1;\
+    for (i=0,j=gap; j<len; i++,j++){\
+    const typeof(A[0]) tmp=A[i];\
+    const int cnd=tmp>A[j];\
+    A[i]=cnd?A[j]:tmp;A[j]=cnd?tmp:A[j];};}while (gap);})
+
 #define reduce(func,arr,len) ({ typeof(arr[0]) accum=arr[0];\
 for(size_t i=1;i<len;i++)accum=func(accum,arr[i]); accum;})
 #define formap(func,arr,len) ({for(size_t i=0;i<len;i++){arr[i]=func(arr[i]);};arr;})
@@ -28,4 +36,5 @@ for(loc=dupfilter(arr,len);loc!=-1;loc=dupfilter(arr,len)){ arr[loc]=rrange(rang
 #define arrcopy(dest,src,len) ({for(size_t i=0;i<len;i++)dest[i]=src[i];dest;})
 #define arr2copy(dest,locs,src,len) ({for(size_t i=0;i<len;i++)dest[locs[i]]=src[i];dest;})
 #define arrcopy2(dest,src,locs,len) ({for(size_t i=0;i<len;i++)dest[i]=src[locs[i]];dest;})
+
 
